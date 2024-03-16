9364124478ec248f8b6eeff6bcba271c5fdc1f1a
anikmondol <anikmondol558363@gmil.com>
2024-03-17 01:22:03 +0600

    introduction to while loop

---
+++ while.js
@@ -0,0 +1,19 @@
+const numbers = 7;
+
+if(numbers < 10){
+    console.log('choto ekta number paisi');
+}
+
+console.log('something new is cooking');
+
+
+// --------while-----loop--------------
+
+let num = 0; // loop variable 
+
+while(num < 5){
+    console.log('loooping', num);
+    // num++;
+    // num += 1;
+    num = num + 1;
+}
