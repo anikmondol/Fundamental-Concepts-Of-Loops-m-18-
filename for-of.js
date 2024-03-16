375b7335fa84acc095c32c8f0171e934f7037dd1
anikmondol <anikmondol558363@gmil.com>
2024-03-17 00:58:46 +0600

    what is a loop, explore of lloop

---
+++ for-of.js
@@ -0,0 +1,16 @@
+const numbers = [10, 22, 35, 77, 89, 54];
+
+
+
+// console.log(numbers);
+
+
+// for(let num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ['orange', 'apple', 'jackfruit', 'watermelon'];
+
+for(let fruit of fruits){
+    console.log(fruit);
+}
