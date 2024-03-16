4ee9b0f6b415219922f985544571bd421a3b582e
anikmondol <anikmondol558363@gmil.com>
2024-03-17 01:47:27 +0600

    problem solving with while loop

---
+++ range.js
@@ -0,0 +1,27 @@
+// let num = 0;
+
+// while(num <= 10){ // focus on the condition 
+//     console.log(num);
+//     num++;
+// }
+
+
+// let num = 50;
+
+// while(num <= 100){
+//     console.log(num);
+//     num++;
+// }
+
+
+let num = 1;
+let sum = 0;
+
+
+while(num <= 10){
+    console.log(num);
+    sum += num;
+    num++;
+}
+
+console.log('sum of number: ',sum);
