696287adfb9ad7f72e148bf93ca7204d6224d976
anikmondol <anikmondol558363@gmil.com>
2024-03-17 02:05:23 +0600

    introduction to for loop

---
+++ odd-even.js
@@ -0,0 +1,20 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+// for(let i = 1; i <= 50; i++){
+//     console.log(i);
+// }
+
+
+// // even
+// for(let i = 2; i <= 20; i += 2){
+//     console.log(i);
+// }
+
+
+// odd 
+for(let i = 1; i <= 20; i += 2){
+    console.log(i);
+}
