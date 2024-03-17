f3c35bc7d1c18b7890c1e95fee531272d6375d40
anikmondol <anikmondol558363@gmil.com>
2024-03-17 14:05:17 +0600

    Fundamental-Concepts-Of-Loops(m-18) done with 95% practic

---
+++ for/task-2.js
@@ -0,0 +1,44 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+// for(let i = 61; i <= 100; i += 2){
+//     console.log(i);
+// }
+
+
+let i = 61;
+
+while(i <= 100){
+    console.log(i);
+    i += 2
+}
+
+
+
+/**
+ 
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ * */ 
+
+
+// for(let i = 78; i <= 98; i+= 2){
+//     console.log(i);
+// }
+
+
+// let i = 78;
+
+// while(i <= 98){
+//     console.log(i);
+//     i += 2;
+// }
